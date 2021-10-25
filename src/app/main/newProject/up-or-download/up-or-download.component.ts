1f51a271e79390a4676eb0be4988a07e80a21a5b
foad <foadsoroush.fs@gmail.com>
2021-10-25 05:30:12 +0330

    edit download documents bill

--- src/app/main/newProject/up-or-download/up-or-download.component.ts
+++ src/app/main/newProject/up-or-download/up-or-download.component.ts
@@ -6,7 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./up-or-download.component.scss']
 })
 export class UpOrDownloadComponent implements OnInit {
-
+  fsList: {
+    docType: string, operatingUnit: number, downloadDate: number,
+    des: string, picture: string
+  }[] = [
+    {
+      'docType': "قرارداد", 'operatingUnit': 67895,
+      'downloadDate': 14000502, 'des': 'شرح لازم پاورقی پیوست',
+      'picture': 'دارد / دانلود'
+    },
+    {
+      'docType': 'شناسه', 'operatingUnit': null
+      , 'downloadDate':null, 'des': ' ',
+      'picture': 'ندارد / آپلود'
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
