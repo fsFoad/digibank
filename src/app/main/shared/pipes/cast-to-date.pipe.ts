6e4a18cef21d6908c558f6b546ba2308f9026130
seyyed <seyyed@local.host>
2021-11-08 01:50:31 +0330

    widen castToDate parameter type to allow number

--- src/app/main/shared/pipes/cast-to-date.pipe.ts
+++ src/app/main/shared/pipes/cast-to-date.pipe.ts
@@ -1,12 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'castToDate'
+    name: 'castToDate'
 })
 export class CastToDatePipe implements PipeTransform {
 
-    transform(args: string): any {
-        if (args)
+    transform(args: number | string): any {
+        if (args) {
             if (args.toString().length === 8) {
                 let tempArgs: string;
                 tempArgs = args.toString().slice(0, 4) + '/' + args.toString().slice(4, 6) + '/' + args.toString().slice(6, 8);
@@ -14,6 +14,7 @@ export class CastToDatePipe implements PipeTransform {
             } else {
                 return null;
             }
+        }
     }
 
 }
