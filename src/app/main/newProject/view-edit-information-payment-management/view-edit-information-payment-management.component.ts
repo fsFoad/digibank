25196b06bb593afc246a4d79c1d289023e663139
foadsoroush <foadsoroush.fs@gmail.com>
2021-11-10 19:54:56 +0330

    create view-edit-information-payment-management.component

---
+++ src/app/main/newProject/view-edit-information-payment-management/view-edit-information-payment-management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-edit-information-payment-management',
+  templateUrl: './view-edit-information-payment-management.component.html',
+  styleUrls: ['./view-edit-information-payment-management.component.scss']
+})
+export class ViewEditInformationPaymentManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
