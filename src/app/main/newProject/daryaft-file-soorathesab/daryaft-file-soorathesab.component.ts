dea30aa6b628249c8bec9371ef3da4fb7a11a3d3
foadsoroush <foadsoroush.fs@gmail.com>
2021-11-30 21:33:20 +0330

    edit dialog

--- src/app/main/newProject/daryaft-file-soorathesab/daryaft-file-soorathesab.component.ts
+++ src/app/main/newProject/daryaft-file-soorathesab/daryaft-file-soorathesab.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Constants} from "../../shared/constants/Constants";
 
 @Component({
   selector: 'app-daryaft-file-soorathesab',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./daryaft-file-soorathesab.component.scss']
 })
 export class DaryaftFileSoorathesabComponent implements OnInit {
+  constants:Constants;
   billingPeriod = 'previousMonth'
   customize = false;
   format = 'html';
