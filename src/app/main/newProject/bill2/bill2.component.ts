e4c12ac4bf938d3f41ecb4e7893988abe4da9fbc
foadsoroush <foadsoroush.fs@gmail.com>
2021-12-02 00:00:09 +0330

    اصلاحات صورتحساب

--- src/app/main/newProject/bill2/bill2.component.ts
+++ src/app/main/newProject/bill2/bill2.component.ts
@@ -11,69 +11,63 @@ export class Bill2Component implements OnInit {
   organizationalUnit = Constants.organizationalUnit;
   tableFlag = false;
   fsList: {
-    accountNumber: number, history: string, branchCode: string,
+    accountNumber: string, history: string, branchCode: string,
     yektaCode: string, dateDue: string, depositor: string, check: string,
     amounts: string, doc: string, amount: number, creditor: string
   }[] = [
     {
-      'accountNumber': 1574655249, 'history': '1400/05/01',
-      'branchCode': '  85456', 'yektaCode': '10000022',
+      'accountNumber': "حساب جاری 65-1000000145-340", 'history': '1400/01/01',
+      'branchCode': '  85456', 'yektaCode': '0',
       'dateDue': '423523452', 'depositor': 'علی محمدی', 'check': '2222589',
-      'amounts': ' 6785656566', 'doc': '5666', 'amount': 7000000, 'creditor': '10000'
+      'amounts': ' 6785656566', 'doc': '5666', 'amount': 10000000, 'creditor': '10000000'
     },
     {
-      'accountNumber': 7541589521, 'history': '1400/05/01'
-      , 'branchCode': '  85456', 'yektaCode': '10000022',
+      'accountNumber': "حساب جاری 65-1000000145-340", 'history': '1400/02/11'
+      , 'branchCode': '  85352', 'yektaCode': '10000023',
       'dateDue': '23423443', 'depositor': 'سینا کریمی', 'check': '2222589',
-      'amounts': ' 356676345', 'doc': '5666', 'amount': 7000000, 'creditor': '10000'
+      'amounts': ' 356676345', 'doc': '5667', 'amount': 7000000, 'creditor': '30000000'
     },
     {
-      'accountNumber': 7548621548, 'history': '1400/05/01'
-      , 'branchCode': '  85456', 'yektaCode': '10000022',
+      'accountNumber': "حساب جاری 65-1000000145-340", 'history': '1400/02/21'
+      , 'branchCode': '  85456', 'yektaCode': '10000024',
       'dateDue': '4542234', 'depositor': 'سامان کاشانی', 'check': '2222589',
-      'amounts': ' 786745656', 'doc': '5666', 'amount': 7000000, 'creditor': '10000'
+      'amounts': ' 786745656', 'doc': '5668', 'amount': 5000000, 'creditor': '15000000'
     },
     {
-      'accountNumber': 1548254962, 'history': '1400/05/01'
-      , 'branchCode': '  85456', 'yektaCode': '10000022',
+      'accountNumber': "حساب جاری 65-1000000145-340", 'history': '1400/04/05'
+      , 'branchCode': '  85352', 'yektaCode': '0',
       'dateDue': '456456345', 'depositor': 'عماد همتی', 'check': '2222589',
-      'amounts': '567678565', 'doc': '5666', 'amount': 7000000, 'creditor': '10000'
+      'amounts': '567678565', 'doc': '5669', 'amount': 25000000, 'creditor': '55000000'
     },
     {
-      'accountNumber': 8465215749, 'history': '1400/05/01'
-      , 'branchCode': '  85456', 'yektaCode': '10000022',
+      'accountNumber': "حساب کوتاه‌مدت 01-1006580523-440", 'history': '1400/05/01'
+      , 'branchCode': '  85352', 'yektaCode': '10000026',
       'dateDue': '34534624', 'depositor': 'فرید ترابی', 'check': '2222589',
-      'amounts': '5645765', 'doc': '5666', 'amount': 7000000, 'creditor': '10000'
+      'amounts': '5645765', 'doc': '5670', 'amount': 1000000, 'creditor': '60000000'
     },
     {
-      'accountNumber': 3256157895, 'history': '1400/05/01'
-      , 'branchCode': '  85456', 'yektaCode': '10000022',
+      'accountNumber': "حساب جاری 65-1000000145-340", 'history': '1400/05/11'
+      , 'branchCode': '  85456', 'yektaCode': '10000027',
       'dateDue': '34556665', 'depositor': 'کامیار کاظمی', 'check': '2222589',
-      'amounts': '789677565', 'doc': '5666', 'amount': 7000000, 'creditor': '10000'
+      'amounts': '789677565', 'doc': '5671', 'amount': 10000000, 'creditor': '25000000'
     },
     {
-      'accountNumber': 7416581213, 'history': '1400/05/01'
-      , 'branchCode': '  85456', 'yektaCode': '10000022',
+      'accountNumber':  "حساب کوتاه‌مدت 01-1006580523-440", 'history': '1400/05/21'
+      , 'branchCode': '  85352', 'yektaCode': '0',
       'dateDue': '35656735434', 'depositor': 'عرفان حشمتی', 'check': '2222589',
-      'amounts': '3545643467', 'doc': '5666', 'amount': 7000000, 'creditor': '10000'
+      'amounts': '3545643467', 'doc': '5672', 'amount': 5000000, 'creditor': '15000000'
     },
     {
-      'accountNumber': 8742154565, 'history': '1400/05/01'
-      , 'branchCode': '  85456', 'yektaCode': '10000022',
-      'dateDue': '3453456567', 'depositor': 'رستم حبیبی', 'check': '2222589',
-      'amounts': ' 4323633', 'doc': '5666', 'amount': 7000000, 'creditor': '10000'
-    },
-    {
-      'accountNumber': 1525844572, 'history': '1400/05/01'
+      'accountNumber':  "حساب کوتاه‌مدت 01-1006580523-440", 'history': '1400/05/23'
       , 'branchCode': '  85456', 'yektaCode': '10000022',
       'dateDue': '34534535676', 'depositor': 'حبیب حسینی', 'check': '2222589',
-      'amounts': '346457658', 'doc': '5666', 'amount': 7000000, 'creditor': '10000'
+      'amounts': '346457658', 'doc': '5666', 'amount': 20000000, 'creditor': '85000000'
     },
     {
-      'accountNumber': 8754788621, 'history': '1400/05/01'
-      , 'branchCode': '  85456', 'yektaCode': '10000022',
+      'accountNumber': "حساب جاری 65-1000000145-340", 'history': '1400/05/29'
+      , 'branchCode': '  85352', 'yektaCode': '10000022',
       'dateDue': '5645675656', 'depositor': 'کریم حسنی', 'check': '2222589',
-      'amounts': ' 24365768', 'doc': '5666', 'amount': 7000000, 'creditor': '10000'
+      'amounts': ' 24365768', 'doc': '5666', 'amount': 16000000, 'creditor': '65000000'
     },
   ];
 
