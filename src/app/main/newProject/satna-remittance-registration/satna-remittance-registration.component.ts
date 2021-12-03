8dc61bd3c6487aaead139e4f7603790d37c56ca9
foadsoroush <foadsoroush.fs@gmail.com>
2021-12-03 19:13:51 +0330

    margin & red color

--- src/app/main/newProject/satna-remittance-registration/satna-remittance-registration.component.ts
+++ src/app/main/newProject/satna-remittance-registration/satna-remittance-registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Constants} from '../../shared/constants/Constants';
 
 @Component({
   selector: 'app-satna-remittance-registration',
@@ -6,12 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./satna-remittance-registration.component.scss']
 })
 export class SatnaRemittanceRegistrationComponent implements OnInit {
-  acItems = [
-    { value: '111', label: 'قرض‌الحسنه ۱۰۰۰۰۲۳۵' },
-    { value: '222', label: 'جاری ۱۰۰۰۰۴۰۰' },
-    { value: '333', label: 'کوتاه‌مدت ۱۰۰۰۰۵۹۸' },
-    { value: '444', label: 'بلندمدت ۱۰۰۰۰۹۷۴' },
-  ]
+  acItems=Constants.acItems;
+  // acItems = [
+  //   { value: '111', label: 'قرض‌الحسنه ۱۰۰۰۰۲۳۵' },
+  //   { value: '222', label: 'جاری ۱۰۰۰۰۴۰۰' },
+  //   { value: '333', label: 'کوتاه‌مدت ۱۰۰۰۰۵۹۸' },
+  //   { value: '444', label: 'بلندمدت ۱۰۰۰۰۹۷۴' },
+  // ]
   
   forItems = [
     { value: '1', label: 'بابت یک' },
