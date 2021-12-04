0c258d330c10bd79e2f956a6a49ada05145e271a
seyyed <seyyed@local.host>
2021-12-04 22:01:04 +0330

    minor changes to tashilat-list

--- src/app/main/newProject/tashilat-list/tashilat-list.component.ts
+++ src/app/main/newProject/tashilat-list/tashilat-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Constants } from 'app/main/shared/constants/Constants';
 
 @Component({
   selector: 'app-tashilat-list',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TashilatListComponent implements OnInit {
     tableFlag = false;
+    unitItems = Constants.organizationalUnit;
     fsList: {
         remaining: number, relevantUnit: string, branch: string,
-        tashilatType: string, fileNumber: string, amount: number,
+        tashilatType: string, fileNumber: string, amount: number, date: number,
         accumulationPaymentObligation: number, installmentsPaid: number
     }[] = [
         {
@@ -19,6 +21,7 @@ export class TashilatListComponent implements OnInit {
             'tashilatType': 'فروش اقساطی',
             'fileNumber': '139911-760',
             'amount': 1000000000,
+            date: 13990424,
             'accumulationPaymentObligation': 1000000,
             'installmentsPaid': 5000000
         },
@@ -29,8 +32,20 @@ export class TashilatListComponent implements OnInit {
             'tashilatType': 'خرید دین',
             'fileNumber': '139910-220',
             'amount': 2000000000,
-            'accumulationPaymentObligation': null,
+            date: 13990217,
+            'accumulationPaymentObligation': 0,
             'installmentsPaid': 1400000000
+        },
+        {
+            'remaining': 643700000,
+            'relevantUnit': 'شرکت1',
+            'branch': 'بانک',
+            'tashilatType': 'خرید دین',
+            'fileNumber': '139908-129',
+            'amount': 2500000000,
+            date: 14000828,
+            'accumulationPaymentObligation': 0,
+            'installmentsPaid': 2000000000
         },
         {
             'remaining': 8465215749,
@@ -39,6 +54,7 @@ export class TashilatListComponent implements OnInit {
             'tashilatType': 'مضاربه',
             'fileNumber': '139932-621',
             'amount': 1500000000,
+            date: 13980116,
             'accumulationPaymentObligation':220000,
             'installmentsPaid': 1000000000
         }];
