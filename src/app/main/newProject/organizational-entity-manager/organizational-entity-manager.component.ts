29dd09fb5e231d7682a02360fb1b82d3c1e22e56
seyyed <seyyed@local.host>
2021-12-05 01:23:45 +0330

    enable next/prev in tabViews

--- src/app/main/newProject/organizational-entity-manager/organizational-entity-manager.component.ts
+++ src/app/main/newProject/organizational-entity-manager/organizational-entity-manager.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./organizational-entity-manager.component.scss']
 })
 export class OrganizationalEntityManagerComponent implements OnInit {
-
+  activeTab = 0;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  nextTab(): void {
+    this.activeTab = Math.min(5, this.activeTab + 1);
+  }
+  prevTab(): void {
+    this.activeTab = Math.max(0, this.activeTab - 1);
+  }
 }
--- src/app/main/newProject/person-editor/person-editor.component.ts
+++ src/app/main/newProject/person-editor/person-editor.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./person-editor.component.scss']
 })
 export class PersonEditorComponent implements OnInit {
-
+  activeTab = 0;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  nextTab(): void {
+    this.activeTab = Math.min(6, this.activeTab + 1);
+  }
+  prevTab(): void {
+    this.activeTab = Math.max(0, this.activeTab - 1);
+  }
 }
