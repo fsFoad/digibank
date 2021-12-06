0ee479c54e76706d8af3cdfa63557ac68ca50e0b
foadsoroush <foadsoroush.fs@gmail.com>
2021-12-06 23:34:38 +0330

    edit wallet

--- src/app/main/newProject/creat-wallet/creat-wallet.component.ts
+++ src/app/main/newProject/creat-wallet/creat-wallet.component.ts
@@ -1,23 +1,47 @@
 import {Component, OnInit} from '@angular/core';
 import {Constants} from '../../shared/constants/Constants';
+import {FormBuilder} from '@angular/forms';
+import {MessageService, NotifierService} from '../../shared/notifier/notifier.module';
 
 @Component({
-    selector: 'app-creat-wallet',
-    templateUrl: './creat-wallet.component.html',
-    styleUrls: ['./creat-wallet.component.scss']
+  selector: 'app-creat-wallet',
+  templateUrl: './creat-wallet.component.html',
+  styleUrls: ['./creat-wallet.component.scss'],
+  providers:[MessageService]
 })
 export class CreatWalletComponent implements OnInit {
-    listDrop: { label: string, value: number }[] = [];
+  listDrop: { label: string, value: number }[] = [];
   acItems = Constants.acItems;
-    constructor() {
-    }
+  display = false;
+  walletForm = this.fb.group({
+    nationalCode: [''],
+    mobile: [''],
+    fullName: [''],
+    charge: [''],
+  });
 
-    ngOnInit(): void {
-        this.listDrop = [
-            {label: 'دریافت رمز پویا ', value: 1},
-            {label: 'خرید پایانه فروشگاهی', value: 2},
-            {label: 'خرید اینترنتی', value: 3},
+  constructor(
+    private fb: FormBuilder,
 
-        ];
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.listDrop = [
+      {label: 'دریافت رمز پویا ', value: 1},
+      {label: 'خرید پایانه فروشگاهی', value: 2},
+      {label: 'خرید اینترنتی', value: 3},
+
+    ];
+  }
+
+  search() {
+    debugger
+    if ((this.walletForm.controls['nationalCode'].value !== null) || (this.walletForm.controls['nationalCode'].value !== undefined)) {
+     debugger
+      this.walletForm.controls['mobile'].patchValue('09192265254');
+      this.walletForm.controls['fullName'].setValue('حامد صبحی');
+      this.walletForm.controls['charge'].setValue(' 100,000ریال');
     }
+  }
 }
