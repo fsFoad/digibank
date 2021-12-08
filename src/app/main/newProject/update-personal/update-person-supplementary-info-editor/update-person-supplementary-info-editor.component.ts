1edb242effea4b785a2cf184aedcd8b72f505821
foad <foadsoroush.fs@gmail.com>
2021-12-08 15:40:25 +0330

    .

--- src/app/main/newProject/update-personal/update-person-supplementary-info-editor/update-person-supplementary-info-editor.component.ts
+++ src/app/main/newProject/update-personal/update-person-supplementary-info-editor/update-person-supplementary-info-editor.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Constants} from '../../../shared/constants/Constants';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-update-person-supplementary-info-editor',
@@ -8,11 +9,36 @@ import {Constants} from '../../../shared/constants/Constants';
 })
 export class UpdatePersonSupplementaryInfoEditorComponent implements OnInit {
   @Input() inputIdentity
+  eduMaghta=Constants.eduMaghta;
+  eduReshte=Constants.eduReshte;
+  eduGraesh=Constants.eduGraesh;
+  eduMoaseseType=Constants.eduMoaseseType;
   cityList = Constants.cityList.filter(x => x.value !== null);
   countryList = Constants.countryList.filter(x => x.value !== null);
-  constructor() {
+  public form: FormGroup;
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      nationalCode: [''],
+      erjaCode: [''],
+      title: [''],
+      eduMaghta: [''],
+      eduReshte: [''],
+      eduGraesh: [''],
+      eduMoaseseType: [''],
+      eduCountry: [''],
+
+
+    });
   }
   ngOnInit(): void {
+    this.form.controls['nationalCode'].patchValue(this.inputIdentity.nationalCode)
+    this.form.controls['erjaCode'].patchValue(this.inputIdentity.erjaCode)
+    this.form.controls['eduMaghta'].patchValue(this.inputIdentity.eduMaghta)
+    this.form.controls['eduReshte'].patchValue(this.inputIdentity.eduReshte)
+    debugger
+    this.form.controls['eduGraesh'].patchValue(this.inputIdentity.eduGraesh)
+    this.form.controls['eduMoaseseType'].patchValue(this.inputIdentity.eduMoaseseType)
+    this.form.controls['eduCountry'].patchValue(this.inputIdentity.country)
   }
 
 }
--- src/app/main/shared/constants/Constants.ts
+++ src/app/main/shared/constants/Constants.ts
@@ -1,12 +1,13 @@
 export interface AccountSummary {
-    title: string;
-    accountNumber: string;
-    balance: number;
-    blockedAmount: number;
-    openingDate: number;
-    openingBranch: string;
-    dateOfLastTurnover: number;
-  }
+  title: string;
+  accountNumber: string;
+  balance: number;
+  blockedAmount: number;
+  openingDate: number;
+  openingBranch: string;
+  dateOfLastTurnover: number;
+}
+
 export class Constants {
   static acSummaries: AccountSummary[] = [
     {
@@ -45,7 +46,7 @@ export class Constants {
       openingBranch: 'تجریش',
       openingDate: 14000716,
     },
-  ]
+  ];
   static branchNames = [
     'تجریش',
     'بازار',
@@ -59,64 +60,63 @@ export class Constants {
     'توحید',
     'میدان آرژانتین',
     'میرداماد',
-  ]
+  ];
   static acItems = [
-    { value: '000', label: '-' },
-    { value: '111', label: 'حساب قرض‌الحسنه 25-1000000589-240' },
-    { value: '222', label: 'حساب جاری 65-1000000145-340' },
-    { value: '444', label: 'حساب جاری 25-1000500148-340' },
-    { value: '333', label: 'حساب کوتاه‌مدت 01-1006580523-440' },
-  ]
+    {value: '000', label: '-'},
+    {value: '111', label: 'حساب قرض‌الحسنه 25-1000000589-240'},
+    {value: '222', label: 'حساب جاری 65-1000000145-340'},
+    {value: '444', label: 'حساب جاری 25-1000500148-340'},
+    {value: '333', label: 'حساب کوتاه‌مدت 01-1006580523-440'},
+  ];
   static organizationalUnit = [
-    { value: '000', label: '-' },
-    { value: '111', label: 'شرکت هلدینگ' },
-    { value: '222', label: 'شرکت تابعه ۱' },
-    { value: '444', label: 'شرکت تابعه ۲' },
-    { value: '333', label: 'شرکت تابعه ۳' },
-  ]
+    {value: '000', label: '-'},
+    {value: '111', label: 'شرکت هلدینگ'},
+    {value: '222', label: 'شرکت تابعه ۱'},
+    {value: '444', label: 'شرکت تابعه ۲'},
+    {value: '333', label: 'شرکت تابعه ۳'},
+  ];
   static shenasnameType = [
-    { value: '0', label: '-' },
-    { value: '1', label: 'المثنی' },
+    {value: '0', label: '-'},
+    {value: '1', label: 'المثنی'},
 
-  ]
+  ];
 
   static contactTypes = [
-    { value: '0',label: '-' },
-    { value: '1',label: 'محل کار' },
-    { value: '2',label: 'محل سکونت' },
+    {value: '0', label: '-'},
+    {value: '1', label: 'محل کار'},
+    {value: '2', label: 'محل سکونت'},
   ];
 
 
-
   static codeHoze = [
-    { value: '0', label: '-' },
-    { value: '1', label: '142' },
-    { value: '2', label: '703' },
+    {value: '0', label: '-'},
+    {value: '1', label: '142'},
+    {value: '2', label: '703'},
 
-  ]
+  ];
   static HavaleInternetiStatus = [
-    { value: '000', label: '-' },
-    { value: '111', label: 'ثبت اولیه' },
-    { value: '222', label: 'در انتظار تایید' },
-    { value: '333', label: 'ارسال شده به بانک' },
-    { value: '444', label: 'تایید نهایی' },
-    { value: '555', label: 'خطا دار' },
-    { value: '666', label: 'لغو شده' },
-  ]
+    {value: '000', label: '-'},
+    {value: '111', label: 'ثبت اولیه'},
+    {value: '222', label: 'در انتظار تایید'},
+    {value: '333', label: 'ارسال شده به بانک'},
+    {value: '444', label: 'تایید نهایی'},
+    {value: '555', label: 'خطا دار'},
+    {value: '666', label: 'لغو شده'},
+  ];
   static referralType = [
-    { value: '000', label: '-' },
-    { value: '111', label: 'عادی' },
-    { value: '222', label: 'دستور پرداخت' },
+    {value: '000', label: '-'},
+    {value: '111', label: 'عادی'},
+    {value: '222', label: 'دستور پرداخت'},
 
-  ]
+  ];
   static forItems = [
-    { value: '000', label: '-' },
-    { value: '111', label: 'حقوق' },
-    { value: '222', label: 'تسویه حساب مالی' },
-    { value: '333', label: 'پرداخت ویزه' },
-    { value: '444', label: 'سایر پرداخت ها' },
+    {value: '000', label: '-'},
+    {value: '111', label: 'حقوق'},
+    {value: '222', label: 'تسویه حساب مالی'},
+    {value: '333', label: 'پرداخت ویزه'},
+    {value: '444', label: 'سایر پرداخت ها'},
 
-  ]
+  ];
   // --------------------------------------------------------------------------------------
   static postalPattern = '\\b(?!(\\d)\\1{3})[13-9]{4}[1346-9][013-9]{5}\\b';
   static phoneNumPattern = '^[0][0-9][0-9]([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]*$)';
@@ -155,198 +155,228 @@ export class Constants {
   static IsFontIran = false;
   static AppFontClass: string = 'isFontIran';
   static AppWrapperInitializer: boolean = true;
-    static notifiSuccess = 'عملیات با موفقیت انجام شد';
-    static licenseInfoList = [
-        {'label': '-', 'value': null},
-        {'label': 'جوازکسب', 'value': 1},
-        {'label': 'جوازتاسیس', 'value': 2},
-        {'label': 'جوازآموزشی', 'value': 3},
-        {'label': 'پروانه تاسیس', 'value': 4},
-        {'label': 'مجوز بسته بندی,توزیع,پخش و تولید', 'value': 5},
-    ];
-    static customerTypes = [
-        {label: 'حقیقی', value: '1'},
-        {label: 'حقوقی', value: '2'},
-    ];
-    static citizenshipTypes = [
-        {label: 'ایرانی', value: '1'},
-        {label: 'غیرایرانی', value: '2'},
-    ];
-    static genderTypes = [
-        {label: '-', value: null},
-        {label: 'مرد', value: '1'},
-        {label: 'زن', value: '2'},
-    ];
-    static  contactTypeGroups = [
-      { label: '-',value: '0'},
-      { label: 'نشانی',value: '1'},
-      { label: 'تلفن ثابت',value: '2'},
-      { label: 'تلفن همراه',value: '3'},
-      { label: 'دورنگار',value: '4'},
-      { label: 'پست الکترونیک',value: '5'},
-      { label: 'صندوق پستی',value: '6'},
-      { label: 'آدرس وب سایت',value: '7'},
-    ];
-    static countryList = [
-        {label: '-', value: null},
-        {label: 'ایران', value: '1'},
-        // {label: 'انگلیس', value: '2'},
-        // {label: 'عربستان', value: '3'},
-    ];
-    static provinceList = [
-        {label: '-', value: null},
-        {label: 'تهران', value: '1', countryvalue: '1'},
-        {label: 'اصفهان', value:'2', countryvalue: '1'},
-        {label: 'فارس', value: '3', countryvalue: '1'},
-        {label: 'خراسان رضوی', value: '4', countryvalue: '1'},
-        {label: 'قم', value: '5', countryvalue: '1'},
-        {label: 'کرمان', value: '6', countryvalue: '1'},
-    ];
-    static cityList = [
-        {label: '-', value: null},
-        {label: 'تهران', value: '1', provincevalue: '1'},
-        {label: 'قم', value: '2', provincevalue: '5'},
-        {label: 'اصفهان', value: '3', provincevalue: '2'},
-        {label: 'شیراز', value: '4', provincevalue: '3'},
-        {label: 'مشهد', value: '5', provincevalue: '4'},
-        {label: 'کرمان', value: '6' , provincevalue: '6'},
-        {label: 'سیرجان', value: '7', provincevalue: '6'},
-        {label: 'رفسنجان', value: '8', provincevalue: '6'},
-        {label: 'جیرفت', value: '9', provincevalue: '6'},
-        {label: 'بم', value: '10', provincevalue: '6'},
-    ];
-    static residenceTypes = [
-        {label: '-', value: null},
-        {label: 'مقیم', value: '1'},
-        {label: 'غیرمقیم', value: '2'},
-    ];
-    static religion = [
-        {label: '-', value: null},
-        {label: 'اسلام', value: '1'},
-        {label: 'مسیح', value: '2'},
-        {label: 'یهود', value: '3'},
-    ];
-    static nahveTasarofs = [
-      { label: '-', value: '0'},
-      { label: 'ملکی', value: '1'},
-      { label: 'استیجاری', value: '2'},
-      { label: 'وقفی', value: '3'},
-    ];
-    static sect = [
-        {label: '-', value: null},
-        {label: 'شیعه', value: '1'},
-        {label: 'سنی', value: '2'},
+  static notifiSuccess = 'عملیات با موفقیت انجام شد';
+  static licenseInfoList = [
+    {'label': '-', 'value': null},
+    {'label': 'جوازکسب', 'value': 1},
+    {'label': 'جوازتاسیس', 'value': 2},
+    {'label': 'جوازآموزشی', 'value': 3},
+    {'label': 'پروانه تاسیس', 'value': 4},
+    {'label': 'مجوز بسته بندی,توزیع,پخش و تولید', 'value': 5},
+  ];
+  static customerTypes = [
+    {label: 'حقیقی', value: '1'},
+    {label: 'حقوقی', value: '2'},
+  ];
+  static citizenshipTypes = [
+    {label: 'ایرانی', value: '1'},
+    {label: 'غیرایرانی', value: '2'},
+  ];
+  static genderTypes = [
+    {label: '-', value: null},
+    {label: 'مرد', value: '1'},
+    {label: 'زن', value: '2'},
+  ];
+  static contactTypeGroups = [
+    {label: '-', value: '0'},
+    {label: 'نشانی', value: '1'},
+    {label: 'تلفن ثابت', value: '2'},
+    {label: 'تلفن همراه', value: '3'},
+    {label: 'دورنگار', value: '4'},
+    {label: 'پست الکترونیک', value: '5'},
+    {label: 'صندوق پستی', value: '6'},
+    {label: 'آدرس وب سایت', value: '7'},
+  ];
+  static countryList = [
+    {label: '-', value: null},
+    {label: 'ایران', value: '1'},
+    // {label: 'انگلیس', value: '2'},
+    // {label: 'عربستان', value: '3'},
+  ];
+  static provinceList = [
+    {label: '-', value: null},
+    {label: 'تهران', value: '1', countryvalue: '1'},
+    {label: 'اصفهان', value: '2', countryvalue: '1'},
+    {label: 'فارس', value: '3', countryvalue: '1'},
+    {label: 'خراسان رضوی', value: '4', countryvalue: '1'},
+    {label: 'قم', value: '5', countryvalue: '1'},
+    {label: 'کرمان', value: '6', countryvalue: '1'},
+  ];
+  static cityList = [
+    {label: '-', value: null},
+    {label: 'تهران', value: '1', provincevalue: '1'},
+    {label: 'قم', value: '2', provincevalue: '5'},
+    {label: 'اصفهان', value: '3', provincevalue: '2'},
+    {label: 'شیراز', value: '4', provincevalue: '3'},
+    {label: 'مشهد', value: '5', provincevalue: '4'},
+    {label: 'کرمان', value: '6', provincevalue: '6'},
+    {label: 'سیرجان', value: '7', provincevalue: '6'},
+    {label: 'رفسنجان', value: '8', provincevalue: '6'},
+    {label: 'جیرفت', value: '9', provincevalue: '6'},
+    {label: 'بم', value: '10', provincevalue: '6'},
+  ];
+  static residenceTypes = [
+    {label: '-', value: null},
+    {label: 'مقیم', value: '1'},
+    {label: 'غیرمقیم', value: '2'},
+  ];
+  static religion = [
+    {label: '-', value: null},
+    {label: 'اسلام', value: '1'},
+    {label: 'مسیح', value: '2'},
+    {label: 'یهود', value: '3'},
+  ];
+  static nahveTasarofs = [
+    {label: '-', value: '0'},
+    {label: 'ملکی', value: '1'},
+    {label: 'استیجاری', value: '2'},
+    {label: 'وقفی', value: '3'},
+  ];
+  static eduMaghta = [
+    {label: '-', value: '0'},
+    {label: 'دکتری', value: '1'},
+    {label: 'کارشناسی ارشد', value: '2'},
+    {label: 'کارشناسی', value: '3'},
+    {label: 'کاردانی', value: '4'},
+    {label: 'دیپلم', value: '5'},
+    {label: 'سیکل', value: '6'},
+  ];
+  static eduReshte = [
+    {value: '0', label: '-'},
+    {value: '1', label: 'کامپیوتر'},
+    {value: '2', label: 'مدیریت'},
 
-    ];
-    static howResidence = [
-        {label: '-', value: null},
-        {label: 'دائم', value: '1'},
-        {label: 'موقت', value: '2'},
-    ];
-    static terminalTypesList = [
-        {value: null, label: '-'},
-        {value: '1', label: 'پایانه فروش متصل به خط تلفن'},
-        {value: '2', label: 'پایانه متصل به خط شبکه'},
-        {value: '3', label: 'پایانه متصل به صندوق فروشگاهی'},
-        {value: '4', label: 'پایانه فروش سیار'},
-        {value: '5', label: 'درگاه پرداخت اینترنتی'},
+  ];
+  static eduGraesh = [
+    {label: '-', value: '0'},
+    {label: 'سخت افزار', value: '1'},
+    {label: 'کار آفرینی', value: '2'},
 
-    ];
-    static segmentTypeList = [
-        {
-            value: null,
-            label: '-'
-        },
-        {
-            value: 1,
-            label: 'نقدی'
-        },
-        {
-            value: 2,
-            label: 'تسهیلات'
-        },
-        {
-            value: 3,
-            label: 'اعتبار'
-        },
-    ];
-    static purchaseTypeList = [
-        {
-            value: null,
-            label: '-'
-        },
-        {
-            value: 1,
-            label: 'خرید فقط از جیب '
-        },
-        {
-            value: 2,
-            label: 'خرید ترکیبی'
-        }
-    ];
-    static chargeTypeList = [
-        {
-            value: null,
-            label: '-'
-        },
-        {
-            value: 1,
-            label: 'شارژ افزایشی'
-        },
-        {
-            value: 2,
-            label: 'شارژ ثابت'
-        }
-    ];
-    static statusList = [
-        {
-            value: null,
-            label: '-'
-        },
-        {
-            value: 1,
-            label: 'فعال'
-        },
-        {
-            value: 2,
-            label: 'غیرفعال'
-        }
-    ];
-    static currencyList = [
-        {
-            value: null,
-            label: '-'
-        },
-        {
-            value: 'IRR',
-            label: 'IRR'
-        }
-    ];
-    static priorityList = [
-        {value: null, label: '-'},
-        {value: 1, label: 'اولویت اول'},
-        {value: 2, label: 'اولویت دوم'},
-        {value: 3, label: 'اولویت سوم'},
-        {value: 4, label: 'اولویت چهارم'},
-        {value: 5, label: 'اولویت پنجم'},
-        {value: 6, label: 'اولویت ششم'},
-        {value: 7, label: 'اولویت هفتم'},
-        {value: 8, label: 'اولویت هشتم'},
-        {value: 9, label: 'اولویت نهم'},
-        {value: 10, label: 'اولویت دهم'},
+  ];
+  static eduMoaseseType = [
+    {label: '-', value: '0'},
+    {label: 'دولتی', value: '1'},
+    {label: 'آزاد', value: '2'},
+    {label: 'غیر انتفاعی', value: '3'},
+    {label: 'پیام نور', value: '4'},
+    {label: 'کارو دانش', value: '5'},
 
-    ];
-    static organizationList = [
-        {value: null, label: '-'},
-        {value: 1, label: 'بانک رسالت'},
-        {value: 2, label: 'سازمان توانیر'},
-        {value: 3, label: 'وزارت بهداشت،درمان و آموزش پزشکی'},
-        {value: 4, label: 'وزارت صنعت، معدن وتجارت'},
-    ];
+  ];
+  static sect = [
+    {label: '-', value: null},
+    {label: 'شیعه', value: '1'},
+    {label: 'سنی', value: '2'},
 
+  ];
+  static howResidence = [
+    {label: '-', value: null},
+    {label: 'دائم', value: '1'},
+    {label: 'موقت', value: '2'},
+  ];
+  static terminalTypesList = [
+    {value: null, label: '-'},
+    {value: '1', label: 'پایانه فروش متصل به خط تلفن'},
+    {value: '2', label: 'پایانه متصل به خط شبکه'},
+    {value: '3', label: 'پایانه متصل به صندوق فروشگاهی'},
+    {value: '4', label: 'پایانه فروش سیار'},
+    {value: '5', label: 'درگاه پرداخت اینترنتی'},
 
-    constructor() {
-        // Constants.notifiSuccess =this.translateService.instant('label.http.status.200');
-
+  ];
+  static segmentTypeList = [
+    {
+      value: null,
+      label: '-'
+    },
+    {
+      value: 1,
+      label: 'نقدی'
+    },
+    {
+      value: 2,
+      label: 'تسهیلات'
+    },
+    {
+      value: 3,
+      label: 'اعتبار'
+    },
+  ];
+  static purchaseTypeList = [
+    {
+      value: null,
+      label: '-'
+    },
+    {
+      value: 1,
+      label: 'خرید فقط از جیب '
+    },
+    {
+      value: 2,
+      label: 'خرید ترکیبی'
     }
+  ];
+  static chargeTypeList = [
+    {
+      value: null,
+      label: '-'
+    },
+    {
+      value: 1,
+      label: 'شارژ افزایشی'
+    },
+    {
+      value: 2,
+      label: 'شارژ ثابت'
+    }
+  ];
+  static statusList = [
+    {
+      value: null,
+      label: '-'
+    },
+    {
+      value: 1,
+      label: 'فعال'
+    },
+    {
+      value: 2,
+      label: 'غیرفعال'
+    }
+  ];
+  static currencyList = [
+    {
+      value: null,
+      label: '-'
+    },
+    {
+      value: 'IRR',
+      label: 'IRR'
+    }
+  ];
+  static priorityList = [
+    {value: null, label: '-'},
+    {value: 1, label: 'اولویت اول'},
+    {value: 2, label: 'اولویت دوم'},
+    {value: 3, label: 'اولویت سوم'},
+    {value: 4, label: 'اولویت چهارم'},
+    {value: 5, label: 'اولویت پنجم'},
+    {value: 6, label: 'اولویت ششم'},
+    {value: 7, label: 'اولویت هفتم'},
+    {value: 8, label: 'اولویت هشتم'},
+    {value: 9, label: 'اولویت نهم'},
+    {value: 10, label: 'اولویت دهم'},
+
+  ];
+  static organizationList = [
+    {value: null, label: '-'},
+    {value: 1, label: 'بانک رسالت'},
+    {value: 2, label: 'سازمان توانیر'},
+    {value: 3, label: 'وزارت بهداشت،درمان و آموزش پزشکی'},
+    {value: 4, label: 'وزارت صنعت، معدن وتجارت'},
+  ];
+
+
+  constructor() {
+    // Constants.notifiSuccess =this.translateService.instant('label.http.status.200');
+
+  }
 }
