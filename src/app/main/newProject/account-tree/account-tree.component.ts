b3a46c16c8ea66d92daa4d51edbf6afd0e986c95
seyyed <seyyed@local.host>
2021-12-11 20:41:17 +0330

    revise account-tree

--- src/app/main/newProject/account-tree/account-tree.component.ts
+++ src/app/main/newProject/account-tree/account-tree.component.ts
@@ -1,18 +1,12 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MenuItem, TreeNode } from 'primeng/api';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 
 interface AccData {
-  type?: string;
-  field1?: string;
-  parentAccount?: string;
-  parentCodeStr: string;
-  fullCode: string;
+  level: 1 | 2 | 3;
   code?: string;
   title?: string;
-  title2?: string;
   active?: boolean;
   nature?: string;
 }
@@ -27,7 +21,7 @@ interface TreeNodeSelectEvent<TData> {
   templateUrl: './account-tree.component.html',
   styleUrls: ['./account-tree.component.scss']
 })
-export class AccountTreeComponent implements OnInit, OnDestroy {
+export class AccountTreeComponent implements OnInit {
   active = false;
   nature: 'debtor' | 'creditor' | 'not-important' = 'not-important';
   accounts: TreeNode[] = createSampleAccounts();
@@ -39,43 +33,53 @@ export class AccountTreeComponent implements OnInit, OnDestroy {
   ];
   constructor(private fb: FormBuilder) {
     this.form = fb.group({
-      type: [''],
-      field1: [''],
-      parentAccount: [''],
-      parentCodeStr: [''],
-      fullCode: [''],
+      level: [0],
       code: ['0'],
       title: [''],
-      title2: [''],
       active: [false],
       nature: ['not-important'],
     });
   }
 
   ngOnInit(): void {
-    this.form.valueChanges.pipe(takeUntil(this.ngDestroy$)).subscribe(value => {
-      if (this.selectedNode) {
-        this.selectedNode.data = value;
-        updateDynamicProperties(this.selectedNode);
-        this.form.patchValue({
-          fullCode: this.selectedNode.data.fullCode,
-        }, { emitEvent: false, });
-      }
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.ngDestroy$.next();
-    this.ngDestroy$.complete();
   }
 
   onNodeSelect(e: TreeNodeSelectEvent<AccData>): void {
-    this.form.setValue(e.node.data);
+    if (e.node.data) {
+      this.form.enable();
+      this.form.setValue(e.node.data);
+    } else {
+      this.form.setValue({ ...createDefaultData(), level: 0 }); // invalid level to hide it in form
+      this.form.disable();
+    }
+  }
+
+  save(): void {
+    const node = this.selectedNode;
+    if (node?.data) {
+      node.data = this.form.value;
+      node.label = `${node.data.code} - ${node.data.title}`;
+    }
   }
 
   addNode(e: any): void {
     const parent = this.selectedNode;
-    const newNode = createNode('0', 'جدید', parent);
+    let level: 1 | 2 | 3;
+    let code: string;
+    if (!parent.data) {
+      level = 1;
+      code = '10';
+    } else if (parent.data.level === 1) {
+      level = 2;
+      code = '1000';
+    } else if (parent.data.level === 2) {
+      level = 3;
+      code = '1000';
+    } else {
+      console.log('cannot add');
+      return;
+    }
+    const newNode = createNode(code, 'جدید', level, parent);
     parent.children = [...(parent.children || []), newNode];
     parent.expanded = true;
     this.selectedNode = newNode;
@@ -83,52 +87,29 @@ export class AccountTreeComponent implements OnInit, OnDestroy {
   }
 }
 
-function updateDynamicProperties(node: TreeNode<AccData>): void {
-  const parentData = node.parent?.data;
-  node.data.fullCode = filterJoin([parentData?.fullCode, node.data.code], '');
-  node.label = filterJoin([node.data.fullCode, node.data?.title], ' - ');
-  for (const child of (node.children || [])) {
-    updateDynamicProperties(child);
-  }
-}
-
-function filterJoin(items: any[], separator?: string, filter?: (x: any) => boolean): string {
-  if (filter) {
-    return items.filter(filter).join(separator);
-  } else {
-    return items.filter(x => x).join(separator);
-  }
-}
-
-
-function createNode(code: string, title: string, parent?: TreeNode<AccData>): TreeNode<AccData> {
-  const parentCodeStr = filterJoin([parent?.data?.parentCodeStr, parent?.data?.code], '');
-  const node: TreeNode<AccData> = {
+function createNode(code: string, title: string, level?: 1 | 2 | 3, parent?: TreeNode<AccData>): TreeNode<AccData> {
+  return {
     parent,
-    data: { ...createDefaultData(), code, title, parentCodeStr, },
+    label: `${code} - ${title}`,
+    data: { ...createDefaultData(), code, title, level },
   };
-  node.label = filterJoin([filterJoin([parentCodeStr, code], ''), title], ' - ');
-  return node;
 }
 
 function createDefaultData(): AccData {
   return {
-    type: '',
-    field1: '',
-    parentAccount: '',
-    parentCodeStr: '',
-    fullCode: '',
+    level: 1,
     code: '',
     title: '',
-    title2: '',
     active: false,
     nature: 'not-important',
   };
 }
 
 function createSampleAccounts(): TreeNode<AccData>[] {
-  const root = createNode('', 'حسابها');
-  root.expanded = true;
+  const root: TreeNode<AccData> = {
+    label: 'حسابها',
+    expanded: true,
+  };
   root.children =
     [
       ['11', 'دارایی‌های جاری'],
@@ -142,8 +123,13 @@ function createSampleAccounts(): TreeNode<AccData>[] {
       ['62', 'سایر هزینه‌ها و درآمدهای غیر عملیاتی'],
       ['91', 'حساب‌های انتظامی'],
     ].map(x => {
-      const node = createNode(x[0], x[1], root);
-      node.children = [createNode('0', 'آزمایشی', node)];
+      const node = createNode(x[0], x[1], 1, root);
+      node.children = [createNode(
+        '1000',
+        'آزمایشی',
+        2,
+        node
+      )];
       return node;
     });
   return [root];
