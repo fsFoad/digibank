c25fffa5a7ffce7c6bea952a34e415d310fb3774
faezehFT <penelopedandelion96ft@gmail.com>
2026-06-19 18:16:56 +0330

    1405/03/29 faezeh1

--- src/app/main/newProject/your-rial-accounts/your-rial-accounts.component.ts
+++ src/app/main/newProject/your-rial-accounts/your-rial-accounts.component.ts
@@ -8,9 +8,76 @@ import { Constants } from 'app/main/shared/constants/Constants';
 })
 export class YourRialAccountsComponent implements OnInit {
   accounts = Constants.acSummaries;
+  level1Rows: L1Row[] = [];
+  level1Loading = false;
   constructor() { }
 
   ngOnInit(): void {
+    this.level1Loading = true;
+    setTimeout(() => {
+      this.level1Rows = createSampleL1Rows();
+      this.level1Loading = false;
+    }, 1000);
   }
+  confirm(): void {
 
+  }
+}
+function createSampleL1Rows(): L1Row[] {
+  return [
+    ['11', 'حساب های بانک ملت'],
+    ['12', 'حساب های بانک ملی'],
+    ['21', 'حساب های بانک سامان'],
+  ].map(bank => ({
+    code: bank[0],
+    title: bank[1],
+
+    rows: [
+      {
+        code: `${bank[0]}01`,
+        title: 'حساب های درآمدی',
+
+        rows: [
+          {
+            code: `${bank[0]}0101`,
+            title: 'کارمزد خدمات بانکی'
+          },
+          {
+            code: `${bank[0]}0102`,
+            title: 'درآمد تسهیلات'
+          },
+          {
+            code: `${bank[0]}0103`,
+            title: 'درآمد صدور ضمانت نامه'
+          }
+        ]
+      },
+
+      {
+        code: `${bank[0]}02`,
+        title: 'حساب های هزینه ای',
+
+        rows: [
+          {
+            code: `${bank[0]}0201`,
+            title: 'هزینه حقوق و دستمزد'
+          },
+          {
+            code: `${bank[0]}0202`,
+            title: 'هزینه اجاره شعب'
+          },
+          {
+            code: `${bank[0]}0203`,
+            title: 'هزینه نگهداری سامانه ها'
+          }
+        ]
+      }
+    ]
+  }));
+}
+
+
+interface L1Row {
+  code: string;
+  title: string;
 }
